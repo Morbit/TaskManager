0a262bfe34dcd12c3d22fd8cb559121c94244b12
Oleg Nayanov <oleg.nayanov@dualbootpartners.com>
2021-08-25 22:04:13 +0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, format: { with: /@/ }, uniqueness: true
 end
