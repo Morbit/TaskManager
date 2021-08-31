e36704dfba50f7b37a4b63ed6b314f05f9294870
Oleg Nayanov <oleg.nayanov@dualbootpartners.com>
2021-08-31 23:10:11 +0400

    added new route

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -1,8 +1,15 @@
-require 'test_helper'
+test "should post create" do
+  password = generate(:string)
+  user = create(:user, { password: password })
+  attrs = {
+    email: user.email,
+    password: password
+  }
+  post :create, params: { session_form: attrs }
+  assert_response :redirect
+end
 
-class Web::SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+test "should delete destroy" do
+  delete :destroy
+  assert_response :redirect
 end
