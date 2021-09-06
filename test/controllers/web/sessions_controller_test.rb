08e20dea52a98c5c1f5894e311e78285a0d31a73
Oleg Nayanov <oleg.nayanov@dualbootpartners.com>
2021-09-07 00:24:29 +0400

    commit review fixes

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -1,15 +1,23 @@
-test "should post create" do
-  password = generate(:string)
-  user = create(:user, { password: password })
-  attrs = {
-    email: user.email,
-    password: password
-  }
-  post :create, params: { session_form: attrs }
-  assert_response :redirect
-end
+require 'test_helper'
 
-test "should delete destroy" do
-  delete :destroy
-  assert_response :redirect
+class Web::SessionsControllerTest < ActionController::TestCase
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  test "should post create" do
+    password = generate(:string)
+    user = create(:user, { password: password })
+    attrs = {
+      email: user.email,
+      password: password
+    }
+    post :create, params: { session_form: attrs }
+    assert_response :redirect
+  end
+  
+  test "should delete destroy" do
+    delete :destroy
+    assert_response :redirect
+  end
 end
