f2e786f01d2a32124fe9df7cf573b9a8e1a0fd8d
Oleg Nayanov <oleg.nayanov@dualbootpartners.com>
2021-11-26 01:31:47 +0400

    added order

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
   def index
-    puts ransack_params
+    sort = 'id desc'
     tasks = Task.all.
       ransack(ransack_params).
       result.
       page(page).
-      per(per_page)
+      per(per_page).
+      order(sort)
 
     respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
   end
